224d09293709d52c7fbc932debf337b669634aa3
J-a-y-a <144910001+J-a-y-a@users.noreply.github.com>
2023-11-11 16:59:23 +0530

    Add project files.

---
+++ EmployeeDetails/Controllers/Employees1Controller.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeDetails;
+
+namespace EmployeeDetails.Controllers
+{
+    public class Employees1Controller : Controller
+    {
+        private EmpployeeDetailsEntities db = new EmpployeeDetailsEntities();
+
+        // GET: Employees1
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+
+        // GET: Employees1/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Employees1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees1/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create( Employee employee,HttpPostedFileBase empImage)
+        {
+            if (ModelState.IsValid)
+            {
+                int a = empImage.ContentLength;
+                employee.Photo = new byte[a];
+                empImage.InputStream.Read(employee.Photo, 0, a);
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees1/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees1/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Employee employee, HttpPostedFileBase empimage)
+        {
+            if (ModelState.IsValid)
+            {
+                int a = empimage.ContentLength;
+                employee.Photo = new byte[a];
+                empimage.InputStream.Read(employee.Photo, 0, a);
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        // GET: Employees1/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EmployeeDetails/Models/Mail.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeDetails.Models
+{
+   
+    
+        public class MailCredentails
+        {
+            public string UserName = "sandujaya8@gmail.com";
+            //public string Password = "Pragati@1949";
+            public string Password = "afagawuitsyvsrxs";
+            //App generated password
+            public string Host = "smtp.gmail.com";
+            public int Port = 587;
+
+        }
+    public class MailModel
+    {
+        public string From
+        {
+            get;
+            set;
+        }
+        public string To
+        {
+            get;
+            set;
+        }
+        public string Subject
+        {
+            get;
+            set;
+        }
+        public string Body
+        {
+            get;
+            set;
+        }
+    }
+}
